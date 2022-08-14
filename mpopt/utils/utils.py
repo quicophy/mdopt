79f4669ef4b7fb5e4b79b275166917a71f74172d
Aleksandr Berezutskii <aleksandr.berezutskii@skoltech.ru>
2022-08-14 17:27:20 +0300

    refactor the utils module

--- mpopt/utils/utils.py
+++ mpopt/utils/utils.py
@@ -1,35 +1,44 @@
-"""
-This module contains relevant utilities.
-"""
+"""This module contains miscellaneous utilities."""
 
 import numpy as np
 from opt_einsum import contract
 
 
-def svd(mat, cut=1e-14, chi_max=1e5, renormalise=False):
-    """
-    Returns the Singular Value Decomposition of a matrix `mat`.
+def svd(
+    mat: np.ndarray,
+    cut: np.float64 = 1e-12,
+    chi_max: np.int32 = 1e4,
+    renormalise: bool = False,
+) -> tuple[np.ndarray]:
+    """Returns the Singular Value Decomposition of a matrix `mat`.
 
     Arguments:
-        mat: np.array[ndim=2]
-            Matrix given as a `np.array` with 2 dimensions.
-        cut: float
+        mat:
+            Matrix given as a `np.ndarray` with 2 dimensions.
+        cut:
             Singular values smaller than this will be discarded.
-        chi_max: int
+        chi_max:
             Maximum number of singular values to keep.
-        renormalise: bool
+        renormalise:
             Renormalisation of the singular value spectrum.
 
     Returns:
-        u_l: ndarray
+        u_l:
             Unitary matrix having left singular vectors as columns.
-        singular_values: ndarray
+        singular_values:
             The singular values, sorted in non-increasing order.
-        v_r: ndarray
+        v_r:
             Unitary matrix having right singular vectors as rows.
     """
 
-    u_l, singular_values, v_r = np.linalg.svd(mat, full_matrices=False)
+    if len(mat.shape) != 2:
+        raise ValueError(
+            f"A valid matrix must have 2 dimensions while the one given has {len(mat.shape)}."
+        )
+
+    u_l, singular_values, v_r = np.linalg.svd(
+        mat, full_matrices=False, compute_uv=True, hermitian=False
+    )
 
     max_num = min(chi_max, np.sum(singular_values > cut))
     ind = np.argsort(singular_values)[::-1][:max_num]
@@ -42,29 +51,35 @@ def svd(mat, cut=1e-14, chi_max=1e5, renormalise=False):
     return u_l, singular_values, v_r
 
 
-def kron_tensors(tensor_1, tensor_2, conjugate_second=False, merge_physicals=True):
-    """
+def kron_tensors(
+    tensor_1: np.ndarray,
+    tensor_2: np.ndarray,
+    conjugate_second: bool = False,
+    merge_physicals: bool = True,
+) -> np.ndarray:
+    """Computes a kronecker product of 2 MPS tensors with different options.
+
     An utility function which is used to compute
     different versions of a kronecker product of 2 MPS tensors.
     This function acts according to the following cartoon.
     Note, that indices `i` and `m` can be merged if `merge_physicals=True`.
 
-          tensor_2
-
+    ```
+           tensor_2
               i
               |                      i
-        j ---()--- k                 |
-                        -->    jl---()---kn = np.array((jl, m, i, kn))
-        l ---()--- n                |
-             |                      m
-             m
-
-          tensor_1
+        j ---( )--- k                |
+                        -->    jl---( )---kn == (jl, m, i, kn)
+        l ---( )--- n                |
+              |                      m
+              m
+           tensor_1
+    ```
 
     Arguments:
-        tensor_1: np.array[ndim=3]
+        tensor_1:
             The first tensor of the product.
-        tensor_2: np.array[ndim=3]
+        tensor_2:
             The second tensor of the product.
         conjugate_second: bool
             Whether to complex-conjugate the second tensor.
@@ -72,12 +87,14 @@ def kron_tensors(tensor_1, tensor_2, conjugate_second=False, merge_physicals=Tru
             Whether to merge physical indices.
     """
 
-    if len(tensor_1.shape) != len(tensor_2.shape):
-        raise ValueError("The tensors must have equal numbers of dimensions.")
-
     if len(tensor_1.shape) != 3:
         raise ValueError(
-            f"The number of dimensions given was {len(tensor_1.shape)},"
+            f"The number of dimensions of the first tensor is {len(tensor_1.shape)},"
+            "but the number of dimensions expected is 3."
+        )
+    if len(tensor_2.shape) != 3:
+        raise ValueError(
+            f"The number of dimensions of the second tensor is {len(tensor_2.shape)},"
             "but the number of dimensions expected is 3."
         )
 
@@ -99,40 +116,52 @@ def kron_tensors(tensor_1, tensor_2, conjugate_second=False, merge_physicals=Tru
     return product
 
 
-def split_two_site_tensor(theta, chi_max=1e5, cut=1e-14, renormalise=False):
+def split_two_site_tensor(
+    tensor: np.ndarray,
+    chi_max: np.int32 = 1e4,
+    cut: np.float64 = 1e-12,
+    renormalise: bool = False,
+) -> tuple:
     """
     Split a two-site MPS tensor as follows:
-          vL --(theta)-- vR     ->    vL --(A)--diag(S)--(B)-- vR
-                |   |                       |             |
-                i   j                       i             j
+
+    ```
+                                                 m         m
+           i ---(tensor)--- l     ->    i ---(A)---diag(S)---(B)--- l
+                 |   |                         |               |
+                 j   k                         j               k
+    ```
 
     Parameters:
-        theta : np.array[ndim=4]
-            Two-site tensor, with legs vL, i, j, vR.
-        chi_max : int
+        tensor :
+            Two-site tensor `(i, j, k, l)`.
+        chi_max :
             Maximum number of singular values to keep.
-        eps : float
+        eps :
             Discard any singular values smaller than eps.
 
     Returns:
-        a_l : np.array[ndim=3]
-            Left isometry on site i, with legs vL, i, vC.
-        singular_values : np.array[ndim=1]
+        a_l :
+            Left isometry `(i, j, m)`.
+        singular_values :
             List of singular values.
-        b_r : np.array[ndim=3]
-            Right isometry on site j, with legs vC, j, vR.
+        b_r :
+            Right isometry `(m, k, l)`.
     """
 
-    # merge the legs to form a matrix to feed into the svd
-    chi_v_l, d_l, d_r, chi_v_r = theta.shape
-    theta = theta.reshape((chi_v_l * d_l, d_r * chi_v_r))
+    if len(tensor.shape) != 4:
+        raise ValueError(
+            "A valid two-site tensor must have 4 legs"
+            f"while the one given has {len(tensor.shape)}."
+        )
 
-    # do a trimmed svd
+    chi_v_l, d_l, d_r, chi_v_r = tensor.shape
+    tensor = tensor.reshape((chi_v_l * d_l, d_r * chi_v_r))
+
     u_l, singular_values, v_r = svd(
-        theta, cut=cut, chi_max=chi_max, renormalise=renormalise
+        tensor, cut=cut, chi_max=chi_max, renormalise=renormalise
     )
 
-    # split legs of u_l and v_r
     chi_v_cut = len(singular_values)
     a_l = u_l.reshape((chi_v_l, d_l, chi_v_cut))
     b_r = v_r.reshape((chi_v_cut, d_r, chi_v_r))
@@ -140,33 +169,37 @@ def split_two_site_tensor(theta, chi_max=1e5, cut=1e-14, renormalise=False):
     return a_l, singular_values, b_r
 
 
-def create_random_mpo(num_sites, bond_dims, phys_dim=2, which="uniform"):
-    """
-    A function which creates a random complex-valued Matrix Product Operator.
+def create_random_mpo(
+    num_sites: np.int32,
+    bond_dimensions: list[int],
+    phys_dim: np.int32,
+    which: str = "uniform",
+) -> list[np.ndarray]:
+    """Creates a random complex-valued Matrix Product Operator.
 
-    The `bond_dims` argument should be given as a list of right virtual dimensions
+    The `bond_dimensions` argument should be given as a list of right virtual dimensions
     without the last trivial virtual dimension. It means, the length of the list is `num_sites - 1`.
 
-    The distributions available in the function: uniform(0,1), normal, random integer {0,1}.
+    The distributions available: `uniform(0,1)`, `normal`, `random integer {0,1}`.
 
     Each tensor in the MPO list has legs (vL, vR, pU, pD), where v stands for "virtual",
     p -- for "physical", and L, R, U, D -- for "left", "right", "up", "down" accordingly.
 
     Arguments:
-        num_sites: int
+        num_sites:
             The number of sites for the MPO.
             This will be equal to the number of tensors.
-        bond_dims: list
+        bond_dimensions:
             A list of bond dimensions.
-        phys_dim: int
+        phys_dim:
             Physical dimension of the tensors.
-        which: str
+        which:
             Specifies the distribution from which
             the matrix elements are being taken.
             Options: "uniform", "normal", "randint".
     """
 
-    bonds = [[dim, dim] for dim in bond_dims]
+    bonds = [[dim, dim] for dim in bond_dimensions]
     bonds.append([1])
     bonds.insert(0, [1])
     bonds = [item for sublist in bonds for item in sublist]
@@ -177,30 +210,31 @@ def create_random_mpo(num_sites, bond_dims, phys_dim=2, which="uniform"):
     ]
 
     if which == "uniform":
-        mps = [
+        mpo = [
             np.random.uniform(size=shapes[i]) + 1j * np.random.uniform(size=shapes[i])
             for i in range(num_sites)
         ]
 
     if which == "normal":
-        mps = [
+        mpo = [
             np.random.normal(size=shapes[i]) + 1j * np.random.normal(size=shapes[i])
             for i in range(num_sites)
         ]
 
     if which == "randint":
-        mps = [
+        mpo = [
             np.random.randint(0, 2, size=shapes[i])
             + 1j * np.random.randint(0, 2, size=shapes[i])
             for i in range(num_sites)
         ]
 
-    return mps
+    return mpo
 
 
-def mpo_to_matrix(mpo, interlace=False, group=False):
-    """
-    A utility function allowing the creation of a matrix from an MPO.
+def mpo_to_matrix(
+    mpo: list[np.ndarray], interlace: bool = False, group: bool = False
+) -> np.ndarray:
+    """Creates a matrix from an MPO.
 
     If `interlace` is `True`, the matrix' legs will go as
     (p0U, p0D, p1U, p1D, ...), which means
@@ -212,26 +246,34 @@ def mpo_to_matrix(mpo, interlace=False, group=False):
     This is done to adjust the matrix to the @ matrix-vector multiplication.
     Note, grouping (if wanted) is being done after the interlacing.
 
+    ```
         p0U p1U p2U
      ___|___|___|___
-    |              | -- example of a 3-site matrix with legs ungrouped.
+    |              | -- example of a 3-site matrix with ungrouped legs.
     |______________|
        |   |   |
       p0D p1D p2D
+    ```
 
     Each tensor in the MPO list has legs (vL, vR, pU, pD), where v stands for "virtual",
     p -- for "physical", and L, R, U, D -- for "left", "right", "up", "down" accordingly.
     Warning: will cause memory overflow for number of sites > ~20.
 
     Arguments:
-        mpo: List of np.ndarray[ndim=4]
+        mpo:
             The MPO to convert to a matrix.
-        interlace: bool
+        interlace:
             Whether to interlace the matrix' legs or not.
-        group: bool
+        group:
             Whether to group the matrix' legs or not.
             Grouping means merging all the up legs into one leg and the same for the down legs.
     """
+
+    for i, tensor in enumerate(mpo):
+        if len(tensor.shape) != 4:
+            raise ValueError(
+                f"A valid MPO tensor must have 4 legs while tensor {i} has {len(tensor.shape)}."
+            )
 
     phys_dim = mpo[0].shape[2]
     num_sites = len(mpo)
@@ -263,8 +305,15 @@ def mpo_to_matrix(mpo, interlace=False, group=False):
     return matrix
 
 
-def mpo_from_matrix(matrix, num_sites, interlaced=True, phys_dim=2, chi_max=1e5):
-    """
+def mpo_from_matrix(
+    mat: np.ndarray,
+    num_sites: np.int32,
+    interlaced: bool = True,
+    phys_dim: np.int32 = 2,
+    chi_max: np.int32 = 1e4,
+) -> list[np.ndarray]:
+    """Creates an MPO from a matrix.
+
     A utility function allowing the creation of a Matrix Product Operator from a matrix.
 
     If `interlaced` is `True`, the matrix' legs are considered to go as
@@ -285,48 +334,47 @@ def mpo_from_matrix(matrix, num_sites, interlaced=True, phys_dim=2, chi_max=1e5)
     p -- for "physical", and L, R, U, D -- for "left", "right", "up", "down" accordingly.
 
     Arguments:
-        matrix: np.array
+        matrix:
             The matrix to convert to an MPO.
             Can be given with either physical legs grouped together or merged.
-        interlaced: bool
+        interlaced:
             Whether the matrix legs are interlaced or not.
-        num_sites: int
+        num_sites:
             The number of sites in the MPO.
-        phys_dim: int
+        phys_dim:
             Local dimension of the physical legs.
-        chi_max: int
+        chi_max:
             Maximum bond dimension allowed in the MPO.
     """
 
-    # Defining some dimensions we will need further
     hilbert_space_dim = phys_dim**num_sites
     phys_dims = [phys_dim] * num_sites * 2
     mps_dim = phys_dim**2
 
     # Checking the matrix has correct shapes for
-    # a given physical dimension and number of sites
-    if (matrix.shape != tuple([hilbert_space_dim] * 2)) and (
-        matrix.shape != tuple(phys_dims)
+    # a given physical dimension and number of sites.
+    if (mat.shape != tuple([hilbert_space_dim] * 2)) and (
+        mat.shape != tuple(phys_dims)
     ):
         raise ValueError(
             f"The matrix' shape should be either {tuple([hilbert_space_dim] * 2)}, "
-            f"or {tuple(phys_dims)}, instead, the matrix given has shape {matrix.shape}."
+            f"or {tuple(phys_dims)}, instead, the matrix given has shape {mat.shape}."
         )
 
-    # Copying the matrix not to change the original one inplace
-    mat = matrix.copy()
+    # Copying the matrix not to change the original one inplace.
+    mat = mat.copy()
 
-    # Reshaping the matrix
-    if matrix.shape == tuple([hilbert_space_dim] * 2):
+    # Reshaping the matrix.
+    if mat.shape == tuple([hilbert_space_dim] * 2):
         mat = mat.reshape(tuple(phys_dims))
 
-    # Dealing with possible interlacing
+    # Dealing with possible interlacing.
     if not interlaced:
         correct_order = list([i + num_sites, i] for i in range(num_sites))
         correct_order = [item for sublist in correct_order for item in sublist]
         mat = mat.transpose(correct_order)
 
-    # Treating the MPO as an MPS with squared physical dimensions
+    # Treating the MPO as an MPS with squared physical dimensions.
     mpo = []
     mat = mat.reshape((-1, mps_dim))
     mat, singular_values, v_r = svd(mat, chi_max=chi_max, renormalise=False)
@@ -341,7 +389,7 @@ def mpo_from_matrix(matrix, num_sites, interlaced=True, phys_dim=2, chi_max=1e5)
         v_r = v_r.reshape((-1, mps_dim, bond_dim))
         mpo.insert(0, v_r)
 
-    # Contracting in the orthogonality centre
+    # Contracting the orthogonality centre.
     mpo[0] = contract(
         "ij, jk, klm",
         mat,
@@ -350,10 +398,11 @@ def mpo_from_matrix(matrix, num_sites, interlaced=True, phys_dim=2, chi_max=1e5)
         optimize=[(0, 1), (0, 1)],
     )
 
-    # Converting the MPS back to the MPO
+    # Converting the MPS back to the MPO.
     mpo = [tensor.transpose((0, 2, 1)) for tensor in mpo]
     mpo = [
         tensor.reshape((tensor.shape[0], tensor.shape[1], phys_dim, phys_dim))
         for tensor in mpo
     ]
+
     return mpo
