90bd78926553dc77999b5ec9f361d96af84d65d8
Aleksandr Berezutskii <aleksandr.berezutskii@skoltech.ru>
2022-11-01 15:04:38 -0400

    new version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2022, Aleksandr Berezutskii"
 author = "Aleksandr Berezutskii"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
