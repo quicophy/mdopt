marginalisation normalisation fix

mdopt/mps/canonical.py
@@ -607,7 +607,9 @@ class CanonicalMPS:
             optimize=[(0, 1), (0, 1)],
         )
 
-    def marginal(self, sites_to_marginalise: List[int], canonicalise: bool = False) -> "CanonicalMPS":  # type: ignore
+    def marginal(
+        self, sites_to_marginalise: List[int], canonicalise: bool = False
+    ) -> "CanonicalMPS":  # type: ignore
         r"""
         Computes a marginal over a subset of sites of an MPS.
         Attention, this method acts inplace. For the non-inplace version,
@@ -684,7 +686,7 @@ class CanonicalMPS:
         # Contracting in the "t" tensors.
         for site in sites_to_marginalise:
             phys_dim = self.phys_dimensions[site]
-            trace_tensor = np.ones((phys_dim,))
+            trace_tensor = np.ones((phys_dim,)) / np.sqrt(phys_dim)
             self.tensors[site] = np.tensordot(self.tensors[site], trace_tensor, (1, 0))
 
         bond_dims = self.bond_dimensions

mdopt/mps/explicit.py
@@ -514,7 +514,11 @@ class ExplicitMPS:
             optimize=[(0, 1), (0, 1)],
         )
 
-    def marginal(self, sites_to_marginalise: List[int], canonicalise: bool = False) -> Union["ExplicitMPS", "CanonicalMPS"]:  # type: ignore
+    def marginal(
+        self,
+        sites_to_marginalise: List[int],
+        canonicalise: bool = False,
+    ) -> Union["ExplicitMPS", "CanonicalMPS"]:  # type: ignore
         r"""
         Computes a marginal over a subset of sites of an MPS.
         Attention, this method does not act inplace, but creates a new object.
@@ -591,7 +595,7 @@ class ExplicitMPS:
         # Contracting in the "t" tensors.
         for site in sites_to_marginalise:
             phys_dim = mps_can.phys_dimensions[site]
-            trace_tensor = np.ones((phys_dim,))
+            trace_tensor = np.ones((phys_dim,)) / np.sqrt(phys_dim)
             mps_can.tensors[site] = np.tensordot(
                 mps_can.tensors[site], trace_tensor, (1, 0)
             )

mdopt/mps/utils.py
@@ -573,7 +573,7 @@ def marginalise(
     # Contracting in the "+" tensors.
     for site in sites_to_marginalise:
         phys_dim = mps.phys_dimensions[site]
-        trace_tensor = np.ones((phys_dim,))
+        trace_tensor = np.ones((phys_dim,)) / np.sqrt(phys_dim)
         mps.tensors[site] = np.tensordot(mps.tensors[site], trace_tensor, (1, 0))
 
     bond_dims = mps.bond_dimensions

tests/mps/test_canonical.py
@@ -534,6 +534,7 @@ def test_canonical_marginal():
         form="Right-canonical",
     )
     mps_marginalised = mps_prod.marginal(sites_to_marginalise)
+    mps_prod_result.tensors[0] /= phys_dim
     for tensor_0, tensor_1 in zip(mps_prod.tensors, mps_prod_result.tensors):
         assert np.isclose(tensor_0, tensor_1).all()
 
@@ -550,7 +551,7 @@ def test_canonical_marginal():
     )
     for tensor in mps_prod_result.tensors:
         tensor *= np.sqrt(phys_dim)
-    mps_prod_result.tensors[0] *= phys_dim ** len(sites_to_marginalise)
+    mps_prod.tensors[0] /= phys_dim
     mps_marginalised = mps_prod.marginal(sites_to_marginalise)
     for tensor_0, tensor_1 in zip(mps_prod.tensors, mps_prod_result.tensors):
         assert np.isclose(tensor_0, tensor_1).all()
@@ -594,7 +595,7 @@ def test_canonical_marginal():
     mps = mps_from_dense(psi, phys_dim=phys_dim, form="Right-canonical")
 
     mps_start = mps.copy()
-    trace_tensor = np.ones(phys_dim)
+    trace_tensor = np.ones(phys_dim) / np.sqrt(phys_dim)
 
     mps_marginalised = mps.marginal(sites_to_marginalise, canonicalise=True)
 

tests/mps/test_explicit.py
@@ -649,7 +649,7 @@ def test_explicit_marginal():
     mps = mps_from_dense(psi, phys_dim=phys_dim, form="Explicit")
 
     mps_start = mps.copy().right_canonical()
-    trace_tensor = np.ones(phys_dim)
+    trace_tensor = np.ones(phys_dim) / np.sqrt(phys_dim)
 
     mps_marginalised = mps.marginal(sites_to_marginalise, canonicalise=False)
 

tests/mps/test_utils.py
@@ -4,6 +4,7 @@ from itertools import combinations
 import pytest
 import numpy as np
 
+from mdopt.mps.explicit import ExplicitMPS
 from mdopt.mps.canonical import CanonicalMPS
 from mdopt.mps.utils import (
     create_state_vector,
@@ -226,7 +227,7 @@ def test_mps_utils_marginalise():
         sites_all = list(range(num_sites))
         sites_to_marginalise = []
         for site in sites_all:
-            if np.random.uniform() < 1 / 2:
+            if np.random.uniform() < 0.5:
                 sites_to_marginalise.append(site)
         sites_left = [site for site in sites_all if site not in sites_to_marginalise]
 
@@ -239,7 +240,10 @@ def test_mps_utils_marginalise():
         mps_marginalised_ex = marginalise(
             mps_explicit, sites_to_marginalise, canonicalise=False
         )
-        assert isinstance(mps_marginalised_ex, CanonicalMPS)
+
+        if sites_all != sites_to_marginalise:
+            assert isinstance(mps_marginalised_e, CanonicalMPS)
+            assert not isinstance(mps_marginalised_ex, ExplicitMPS)
 
         with pytest.raises(ValueError):
             mps_right.marginal([100, 200])