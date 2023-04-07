fef0f61dc98c065f35b8b8f986c1dd7305ccdf99
meandmytram <berezutskii@phystech.edu>
2023-04-07 18:35:48 -0400

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../"))
 # -- Project information -----------------------------------------------------
 
 project = "mdopt"
-copyright = "2022, Aleksandr Berezutskii"
+copyright = "2023, Aleksandr Berezutskii"
 author = "Aleksandr Berezutskii"
 
 # The full version, including alpha/beta/rc tags
