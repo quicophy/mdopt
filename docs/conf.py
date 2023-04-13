aa40b64b56e5450395528793ca13b268b1e33789
meandmytram <berezutskii@phystech.edu>
2023-04-13 13:25:20 -0400

    docs config update

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+nbsphinx_allow_errors = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
