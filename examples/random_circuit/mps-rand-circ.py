b48b3d5512c3361eed7463196baaa692a50cc0e6
Aleksandr Berezutskii <bereza@cedar1.cedar.computecanada.ca>
2024-04-14 23:15:15 -0700

    scripts for cedar

--- examples/random_circuit/mps-rand-circ.py
+++ examples/random_circuit/mps-rand-circ.py
@@ -1,9 +1,32 @@
+"""This script is used to launch calculations on Compute Canada clusters."""
+
+import os
+import sys
+import logging
 import numpy as np
-from tqdm import tqdm
 from scipy.stats import unitary_group
-from mdopt.mps.utils import create_simple_product_state
-from mdopt.contractor.contractor import mps_mpo_contract
-from examples.random_circuit.random_circuit import create_mpo_from_layer
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Append paths using environment variables or hardcoded fallbacks
+project_root = os.getenv(
+    'MDOPT_PATH', '/home/bereza/projects/def-ko1/bereza/project-mdopt/mdopt'
+    )
+examples_path = os.getenv(
+    'MDOPT_EXAMPLES_PATH', '/home/bereza/projects/def-ko1/bereza/project-mdopt/mdopt/examples'
+    )
+
+sys.path.append(project_root)
+sys.path.append(examples_path)
+
+try:
+    from mdopt.mps.utils import create_simple_product_state
+    from mdopt.contractor.contractor import mps_mpo_contract
+    from examples.random_circuit.random_circuit import create_mpo_from_layer
+except ImportError as e:
+    logging.error("Failed to import required modules. Ensure paths are correct.", exc_info=True)
+    sys.exit(1)
 
 PHYS_DIM = 2
 circ_depths = [4, 6, 8, 10, 12, 14, 16, 18, 20]
@@ -13,7 +36,7 @@ num_qubits = [27, 81, 243]
 tails = {}
 for NUM_QUBITS in num_qubits:
     for BOND_DIM in bond_dims:
-        for NUM_LAYERS_CIRC in tqdm(circ_depths):
+        for NUM_LAYERS_CIRC in circ_depths:
             tails_iter = []
             state = create_simple_product_state(
                 num_sites=NUM_QUBITS,
@@ -24,9 +47,7 @@ for NUM_QUBITS in num_qubits:
             )
 
             for k in range(NUM_LAYERS_CIRC):
-                layer = unitary_group.rvs(
-                    PHYS_DIM**2, size=NUM_QUBITS // PHYS_DIM - k % 2
-                )
+                layer = unitary_group.rvs(PHYS_DIM**2, size=NUM_QUBITS // PHYS_DIM - k % 2)
                 mpo = create_mpo_from_layer(layer)
                 state = mps_mpo_contract(
                     mps=state,
@@ -50,22 +71,9 @@ for NUM_QUBITS in num_qubits:
                 fidels = [1 - error for error in errors]
                 tails_iter.append(np.prod(fidels))
 
-            tails[
-                "numqubits"
-                + str(NUM_QUBITS)
-                + "_bonddim"
-                + str(BOND_DIM)
-                + "_circlayers"
-                + str(NUM_LAYERS_CIRC)
-            ] = tails_iter
+            tails_key = f"data/numqubits{NUM_QUBITS}_bonddim{BOND_DIM}_circlayers{NUM_LAYERS_CIRC}"
+            tails[tails_key] = tails_iter
 
-            np.save(
-                "numqubits"
-                + str(NUM_QUBITS)
-                + "_bonddim"
-                + str(BOND_DIM)
-                + "_circlayers"
-                + str(NUM_LAYERS_CIRC)
-                + ".npy",
-                tails,
-            )
+            np.save(f"{tails_key}.npy", tails)
+
+logging.info("Calculation completed successfully.")
---
+++ examples/random_circuit/submit-job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --time=02:00:00            # Time limit (hh:mm:ss)
+#SBATCH --cpus-per-task=8          # Number of CPU cores per task
+#SBATCH --mem=32000                # Memory per node
+#SBATCH --job-name=random-circuit  # Descriptive job name
+#SBATCH --output=%x-%j.out         # Standard output and error log
+
+module load python/3.11.5
+source ~/envs/myenv/bin/activate
+
+python mps-rand-circ.py
