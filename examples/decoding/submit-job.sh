1640dda977dbe7b627dd27e1200aeca664a6fd1f
Aleksandr (Sasha) Berezutskii <aleksandr.berezutskii@usherbrooke.ca>
2024-04-23 09:38:28 -0700

    update scripts

--- examples/decoding/submit-job.sh
+++ examples/decoding/submit-job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=12:00:00                     # Time limit (hh:mm:ss)
-#SBATCH --cpus-per-task=16                  # Number of CPU cores per task
-#SBATCH --mem=256000                        # Memory per node
+#SBATCH --time=48:00:00                     # Time limit (hh:mm:ss)
+#SBATCH --cpus-per-task=8                   # Number of CPU cores per task
+#SBATCH --mem=128000                        # Memory per node
 #SBATCH --job-name=decoding-classical-ldpc  # Descriptive job name
 #SBATCH --output=%x-%j.out                  # Standard output and error log
 
