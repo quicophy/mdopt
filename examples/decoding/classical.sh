4473b911bd3248fbc4c7fe6ef195259460274d18
meandmytram <berezutskii@phystech.edu>
2024-05-22 14:17:44 -0400

    classical decoding update

--- examples/decoding/classical.sh
+++ examples/decoding/classical.sh
@@ -13,34 +13,47 @@ source ~/envs/myenv/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index numpy scipy opt_einsum tqdm qecstruct more_itertools networkx matrex@git+https://github.com/quicophy/matrex
 
-# Define arrays of system sizes and bond dimensions
+# Define arrays of system sizes, bond dimensions and error probabilities
 system_sizes=(96 192 384)
-bond_dims=(128 256 512)
+bond_dims=(128 256 512 1024)
+
+error_probabilities=()
+start=0.1
+end=0.3
+num_points=10
+step=$(echo "($end - $start) / ($num_points - 1)" | bc -l)
+for ((i=0; i<$num_points; i++))
+do
+    value=$(echo "$start + $i * $step" | bc -l)
+    error_probabilities+=($value)
+done
 
 # Create job submission scripts by iterating
 # over each combination of system_size and bond_dim and submitting them
 for system_size in "${system_sizes[@]}"; do
     for bond_dim in "${bond_dims[@]}"; do
-        # Create a job submission script for each combination
-        cat > "submit-job-${system_size}-${bond_dim}.sh" <<EOS
+        for error_prob in "${error_probabilities[@]}"; do
+            # Create a job submission script for each combination
+            cat > "submit-job-${system_size}-${bond_dim}-${error_prob}.sh" <<EOS
 #!/bin/bash
-#SBATCH --time=120:00:00                                                    # Time limit (hh:mm:ss)
-#SBATCH --cpus-per-task=1                                                   # Number of CPU cores per task
-#SBATCH --mem=16000                                                         # Memory per node
-#SBATCH --job-name=decoding-classical-ldpc-${system_size}-${bond_dim}       # Descriptive job name
-#SBATCH --output=decoding-classical-ldpc-${system_size}-${bond_dim}-%j.out  # Standard output and error log
+#SBATCH --time=10:00:00                                                                   # Time limit (hh:mm:ss)
+#SBATCH --cpus-per-task=1                                                                 # Number of CPU cores per task
+#SBATCH --mem=16000                                                                       # Memory per node
+#SBATCH --job-name=decoding-classical-ldpc-${system_size}-${bond_dim}-${error_prob}       # Descriptive job name
+#SBATCH --output=decoding-classical-ldpc-${system_size}-${bond_dim}-${error_prob}-%j.out  # Standard output and error log
 
 module load python/3.11.5
 source ~/envs/myenv/bin/activate
 
 # Run the Python script with the specified system size and bond dimension
-python examples/decoding/classical.py --system_size $system_size --bond_dim $bond_dim
+python examples/decoding/classical.py --system_size $system_size --bond_dim $bond_dim --error_prob $error_prob
 EOS
 
-        # Submit the job
-        echo "Submitting the job for system size ${system_size} and bond dimension ${bond_dim}"
-        sbatch "submit-job-${system_size}-${bond_dim}.sh" --export=system_size=$system_size,bond_dim=$bond_dim
+            echo "Submitting the job for system size ${system_size}, bond dimension ${bond_dim} and probability error ${error_prob}"
+            sbatch "submit-job-${system_size}-${bond_dim}-${error_prob}.sh" --export=system_size=$system_size,bond_dim=$bond_dim,error_prob=$error_prob
+        done
     done
 done
 
 echo "All jobs have been submitted. Check the queue with 'squeue -u \${USER}'"
+
