61529539f1c94f8cfbb15965a7bff8060c769d2f
meandmytram <berezutskii@phystech.edu>
2024-08-28 19:05:19 -0400

    add decoding of csp codes

--- examples/decoding/quantum_csp.py
+++ examples/decoding/quantum_csp.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 import logging
 import argparse
 import numpy as np
@@ -39,3 +40,153 @@ examples_path_narval = os.getenv(
 
 sys.path.append(project_root_graham)
 sys.path.append(examples_path_graham)
+
+try:
+    from examples.decoding.decoding import (
+        decode_css,
+        pauli_to_mps,
+        generate_pauli_error_string,
+    )
+except ImportError as e:
+    logging.error(
+        "Failed to import required modules. Ensure paths are correct.", exc_info=True
+    )
+    sys.exit(1)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Launch quantum CSP code decoding on Compute Canada clusters."
+    )
+    parser.add_argument(
+        "--num_qubits",
+        type=int,
+        nargs="+",
+        required=True,
+        help="The number of qubits in the code.",
+    )
+    parser.add_argument(
+        "--bond_dim",
+        type=int,
+        required=True,
+        help="Maximum bond dimension to keep during contraction.",
+    )
+    parser.add_argument(
+        "--error_rate",
+        type=float,
+        nargs="+",
+        required=True,
+        help="The error rate.",
+    )
+    parser.add_argument(
+        "--num_experiments",
+        type=int,
+        required=True,
+        help="The number of experiments to run.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        required=True,
+        help="The seed for the random number generator.",
+    )
+    parser.add_argument(
+        "--batch",
+        type=int,
+        required=True,
+        help="The batch to process.",
+    )
+    return parser.parse_args()
+
+
+def run_experiment(
+    batch, num_qubits, code_file, bond_dim, error_rate, num_experiments, seed
+):
+    logging.info(
+        f"Starting experiments for Batch={batch}, Qubits={num_qubits}, Code={code_file}, BOND_DIM={bond_dim}, ERROR_RATE={error_rate}, SEED={seed}"
+    )
+
+    with open(code_file, "r") as code_json:
+        code_data = json.load(code_json)
+        x_code = qec.LinearCode(
+            qec.BinaryMatrix(
+                num_columns=code_data["num_qubits"], rows=code_data["x_stabs"]
+            )
+        )
+        z_code = qec.LinearCode(
+            qec.BinaryMatrix(
+                num_columns=code_data["num_qubits"], rows=code_data["z_stabs"]
+            )
+        )
+        quantum_csp_code = qec.CssCode(x_code=x_code, z_code=z_code)
+
+    seed_seq = np.random.SeedSequence(seed)
+    failures = []
+
+    for l in tqdm(range(num_experiments)):
+        new_seed = seed_seq.spawn(1)[0]
+        rng = np.random.default_rng(new_seed)
+        random_integer = rng.integers(1, 10**8 + 1)
+        experiment_seed = random_integer
+
+        try:
+            error = generate_pauli_error_string(
+                len(quantum_csp_code),
+                error_rate,
+                seed=experiment_seed,
+                error_model="Depolarising",
+            )
+            error = pauli_to_mps(error)
+
+            _, success = decode_css(
+                code=quantum_csp_code,
+                error=error,
+                chi_max=bond_dim,
+                bias_type="Depolarising",
+                bias_prob=error_rate,
+                renormalise=True,
+                silent=True,
+                contraction_strategy="Optimised",
+            )
+
+            failures.append(1 - success)
+        except Exception as e:
+            logging.error(f"Experiment {l} failed with error: {str(e)}", exc_info=True)
+            failures.append(1)
+
+    # Store results in a structured filename
+    result_filename = (
+        f"numqubits{num_qubits}_bonddim{bond_dim}_errorrate{error_rate:.12f}_"
+        f"seed{seed}_batch{batch}_{os.path.splitext(os.path.basename(code_file))[0]}.npy"
+    )
+    np.save(result_filename, np.array(failures))
+
+    logging.info(
+        f"Completed experiments for {result_filename} with {np.mean(failures)*100:.2f}% failure rate."
+    )
+
+
+def main():
+    args = parse_arguments()
+
+    for num_qubits in args.num_qubits:
+        code_path = f"data-csp-codes/batch_{args.batch}/codes/qubits_{num_qubits}"
+
+        for code in os.listdir(code_path):
+            if code.endswith(".json"):
+                code_file = os.path.join(code_path, code)
+
+                for error_rate in args.error_rate:
+                    run_experiment(
+                        batch=args.batch,
+                        num_qubits=num_qubits,
+                        code_file=code_file,
+                        bond_dim=args.bond_dim,
+                        error_rate=error_rate,
+                        num_experiments=args.num_experiments,
+                        seed=args.seed,
+                    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/decoding/quantum_csp.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Load the Python module
+module load python/3.11.5
+
+# Check if the virtual environment exists, if not, create and activate it
+if [ ! -d "$HOME/envs/myenv" ]; then
+    virtualenv --no-download "$HOME/envs/myenv"
+fi
+source "$HOME/envs/myenv/bin/activate"
+
+# Install required Python packages if they are not already installed
+pip install --no-index --upgrade pip
+pip install --no-index numpy scipy opt_einsum tqdm qecstruct more_itertools networkx matrex@git+https://github.com/quicophy/matrex
+
+# Define arrays of qubit sizes, bond dimensions, error rates, and batches
+nums_qubits=(30 40 50 60 70 80 90 100)          # Number of qubits to scan
+bond_dims=(8 16 32)                             # Bond dimensions to scan
+seeds=(123 124 125 126 127 128 129 130 131 132) # 10 random seeds
+num_experiments=10                              # Per each random seed
+batches=(1 2 3 4 5 6 7 8 9 10 11 12 13 14)      # Batches from 1 to 14
+
+error_rates=()
+start=0.01
+end=0.5
+num_points=10
+step=$(echo "($end - $start) / ($num_points - 1)" | bc -l)
+for ((i=0; i<$num_points; i++))
+do
+    value=$(echo "$start + $i * $step" | bc -l)
+    error_rates+=($value)
+done
+
+# Create job submission scripts by iterating over
+# each combination of seed, batch, num_qubits, bond_dim, and error_rate and submitting them
+for seed in "${seeds[@]}"; do
+    for num_qubits in "${nums_qubits[@]}"; do
+        for bond_dim in "${bond_dims[@]}"; do  # Iterate over bond dimensions
+            for error_rate in "${error_rates[@]}"; do
+                for batch in "${batches[@]}"; do  # Iterate over batches
+                    # Create a job submission script for each combination
+                    cat > "submit-job-batch${batch}-qubits${num_qubits}-bond${bond_dim}-error${error_rate}-seed${seed}.sh" <<EOS
+#!/bin/bash
+#SBATCH --time=12:00:00                                                                                               # Time limit (hh:mm:ss)
+#SBATCH --cpus-per-task=1                                                                                             # Number of CPU cores per task
+#SBATCH --mem=16000                                                                                                   # Memory per node
+#SBATCH --job-name=decoding-csp-batch${batch}-qubits${num_qubits}-bond${bond_dim}-error${error_rate}-seed${seed}      # Descriptive job name
+#SBATCH --output=decoding-csp-batch${batch}-qubits${num_qubits}-bond${bond_dim}-error${error_rate}-seed${seed}-%j.out # Standard output and error log
+
+module load python/3.11.5
+source "$HOME/envs/myenv/bin/activate"
+
+# Run the Python script with the specified parameters
+python quantum_csp.py --num_qubits ${num_qubits} --bond_dim ${bond_dim} --error_rate ${error_rate} --num_experiments ${num_experiments} --seed ${seed} --batch ${batch}
+EOS
+                    echo "Submitting the job for batch ${batch}, qubit size ${num_qubits}, bond dimension ${bond_dim}, error rate ${error_rate}, and seed ${seed}."
+                    sbatch "submit-job-batch${batch}-qubits${num_qubits}-bond${bond_dim}-error${error_rate}-seed${seed}.sh"
+                    rm "submit-job-batch${batch}-qubits${num_qubits}-bond${bond_dim}-error${error_rate}-seed${seed}.sh"
+                done
+            done
+        done
+    done
+done
+
+echo "All jobs have been submitted. Check the queue with 'squeue -u \${USER}'"
