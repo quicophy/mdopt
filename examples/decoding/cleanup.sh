4333fa5c74ab031b6322edb1fb931b5fbcf409a2
Aleksandr (Sasha) Berezutskii <aleksandr.berezutskii@usherbrooke.ca>
2024-11-14 21:55:04 -0500

    Update cleanup.sh

--- examples/decoding/cleanup.sh
+++ examples/decoding/cleanup.sh
@@ -5,6 +5,6 @@ find . -type f -name "*.out" -exec rm -f {} \;
 
 # Move all files containing "bonddim" in the file name to the "data" folder
 mkdir -p data
-find . -type f -name "*bonddim*" -exec mv {} data/ \;
+find . -type f -name "*_bonddim*" -exec mv {} data/ \;
 
 echo "All .out files deleted and files containing 'bonddim' moved to the 'data' folder."
