cd2bcc4e93b2cd7511835acc4d540208883f18c0
meandmytram <berezutskii@phystech.edu>
2025-10-28 15:48:30 -0400

    remove logo background

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # This will delete the annoying 'bases: object' line from the docs.
 
