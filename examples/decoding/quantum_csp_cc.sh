1edaa32accb9f5b94fb937de5fee12231d1e5a14
meandmytram <berezutskii@phystech.edu>
2026-01-25 20:33:48 -0500

    update cc script

--- examples/decoding/quantum_csp_cc.sh
+++ examples/decoding/quantum_csp_cc.sh
@@ -14,6 +14,22 @@ pip install --no-index --upgrade pip
 pip install --no-index numpy scipy opt_einsum tqdm qecstruct more_itertools networkx
 pip install git+ssh://git@github.com/quicophy/matrex.git
 
+#batches=(9)                                        # Array of batches
+#nums_qubits=(30)                                   # Array of numbers of qubits
+#code_ids=(5 8 9 17 24 30 35 39 44 77 84 87 88 91)  # Array of code IDs (max 99)
+#nums_qubits=(40)
+#code_ids=(0 1 2 3 4 6 10 11 13 14 19 20 21 22 24 25 28 29 30 31 33 35 36 37 39 41 43 44 45 46 47 48 49 50 51 52 54 55 56 57 58 59 60 61 63 65 66 67 68 69 70 71 72 75 76 77 78 79 80 81 84 85 86 89 90 91 94 95 96 97 98 99)
+#nums_qubits=(50)
+#code_ids=({0..99})
+#nums_qubits=(60)
+#code_ids=(0 1 2 3 4 5 6 7 8 9 12 13 14 15 16 17 18 19 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 47 49 50 51 52 54 55 56 57 58 59 60 64 66 67 69 70 71 72 73 74 76 77 78 79 80 81 85 86 87 88 89 90 91 93 94 96 97 99)
+#nums_qubits=(70)
+#code_ids=(16 20 22 25 29 30 32 33 39 41 45 46 47 48 54 59 61 62 64 67 72 73 78 80 85 87 95 98)
+#nums_qubits=(80)
+#code_ids=(1 3 7 25 35 38 42 47 50 53 74 79)
+#nums_qubits=(90)
+#code_ids=(17 24 30)
+
 batches=({1..14})                                # Array of batches
 nums_qubits=(30)                                 # Array of numbers of qubits
 code_ids=({0..99})                               # Array of code IDs
