bb3d9da2192f422af50d1f6dfb1dd9fd9766fb82
meandmytram <berezutskii@phystech.edu>
2026-03-08 11:03:02 +0000

    update quantum_csp_cc.sh

--- examples/decoding/quantum_csp_cc.sh
+++ examples/decoding/quantum_csp_cc.sh
@@ -14,7 +14,7 @@ pip install --no-index --upgrade pip
 pip install --no-index numpy scipy opt_einsum tqdm qecstruct more_itertools networkx
 pip install git+ssh://git@github.com/quicophy/matrex.git
 
-#batches=(9)                                        # Array of batches
+batches=(9)                                         # Array of batches
 #nums_qubits=(30)                                   # Array of numbers of qubits
 #code_ids=(5 8 9 17 24 30 35 39 44 77 84 87 88 91)  # Array of code IDs (max 99)
 #nums_qubits=(40)
@@ -23,19 +23,21 @@ pip install git+ssh://git@github.com/quicophy/matrex.git
 #code_ids=({0..99})
 #nums_qubits=(60)
 #code_ids=(0 1 2 3 4 5 6 7 8 9 12 13 14 15 16 17 18 19 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 47 49 50 51 52 54 55 56 57 58 59 60 64 66 67 69 70 71 72 73 74 76 77 78 79 80 81 85 86 87 88 89 90 91 93 94 96 97 99)
-#nums_qubits=(70)
-#code_ids=(16 20 22 25 29 30 32 33 39 41 45 46 47 48 54 59 61 62 64 67 72 73 78 80 85 87 95 98)
+nums_qubits=(70)
+code_ids=(16 20 22 25 29 30 32 33 39 41 45 46 47 54 59 61 62 64 67 72 73 78 80 85 87 95 98)
 #nums_qubits=(80)
 #code_ids=(1 3 7 25 35 38 42 47 50 53 74 79)
 #nums_qubits=(90)
 #code_ids=(17 24 30)
 
-batches=({1..14})                                # Array of batches
-nums_qubits=(30)                                 # Array of numbers of qubits
-code_ids=({0..99})                               # Array of code IDs
-bond_dims=(150)                                  # Array of bond dimensions
-seeds=(123)                                      # Array of random seeds
-num_experiments=5000                             # Runs per each random seed
+bond_dims=(400)                                  # Array of bond dimensions
+seeds=(                                          # Array of random seeds
+    0 1 2 3 4 5 6 7 8 9
+    10 11 12 13 14 15 16 17 18 19
+    20 21 22 23 24 25 26 27 28 29
+    30 31 32 33 34 35 36
+)
+num_experiments=10000                            # Runs per each random seed
 error_model="Bitflip"                            # The error model
 bias_probs=(1e-3)                                # Array of decoder bias probabilities
 tolerances=(0)                                   # Array of numerical tolerances for the MPS within the decoder
@@ -43,17 +45,8 @@ cuts=(0)                                         # Array of SVD cut-offs for the
 num_processes=16                                 # The number of processes to use in parallel
 silent=false                                     # Whether to suppress the output of the Python script
 
-error_rates=()
-start=0.01
-end=0.21
-step=0.02
-current=$start
-while (( $(echo "$current <= $end" | bc -l) ))
-do
-    error_rates+=($current)
-    current=$(echo "$current + $step" | bc -l)
-done
-error_rates=(0.0001 0.001 0.002 0.004 0.008 0.01)
+error_rates=(0.0001)
+
 # Iterate over combinations of the arguments and run the Python script
 for seed in "${seeds[@]}"; do
     for num_qubits in "${nums_qubits[@]}"; do
@@ -69,9 +62,9 @@ for seed in "${seeds[@]}"; do
                                     # Create the job submission script
                                     cat > "$job_script" <<EOS
 #!/bin/bash
-#SBATCH --time=12:00:00                                                                                         # Time limit (hh:mm:ss)
+#SBATCH --time=06:00:00                                                                                         # Time limit (hh:mm:ss)
 #SBATCH --cpus-per-task=${num_processes}                                                                        # Number of CPU cores per task
-#SBATCH --mem=32000                                                                                             # Memory per node
+#SBATCH --mem=30000                                                                                             # Memory per node
 #SBATCH --job-name=csp-${num_qubits}-${bond_dim}-${error_rate}-${error_model}-${batch}-${code_id}-${seed}       # Descriptive job name
 #SBATCH --output=csp-${num_qubits}-${bond_dim}-${error_rate}-${error_model}-${batch}-${code_id}-${seed}-%j.out  # Standard output and error log
 
